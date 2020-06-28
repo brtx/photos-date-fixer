1d41ef5f46c9baefcaa44a7a1c31fee183306953
Stefan Barthel <stefan.barthel@zuehlke.com>
2020-06-28 22:24:02 +0200

    cleanup. only one call to jxa. set date.

--- index.js
+++ index.js
@@ -8,37 +8,25 @@ let db = new sqlite3.Database('/Users/bas/Pictures/Photos Library.photoslibrary/
   console.log('Connected to the photos database.');
 });
 
-// // 366570014 backup date created 13.08.2012 19:00:14
-// // 366570014000
-// // 0 => 01.01.2001 1:00:00
-// // -31622400 => 01.01.2000 1:00:00
-// //  31536000 => 01.01.2002 1:00:00
-
 function getDateFromAppleDate(appleDate) {
   return new Date(Date.parse(new Date(2001, 1, 1, 1, 0, 0)) + appleDate * 1000);
 }
 
-function setDate(id, date) {
-  // console.log("Date: " + date);
-  return osa((_id, _date) => {
+function setDate(rows) {
+  return osa((_rows) => {
     const app = Application("Photos");
-    const item = app.mediaItems.byId(_id);
-    console.log(item.filename() + "\t" + item.date() + "\t" + new Date(_date));
 
-    // const selectedPhoto = app.selection()[0];    
-    // selectedPhoto.date = new Date(_date);
-  })(id, date)
+    var i = 1;
+    _rows.forEach(row => {
+      const item = app.mediaItems.byId(row.id);
+      console.log(i++ + "," + item.filename() + "," + item.date() + "," + new Date(row.newDate));
+      item.date = new Date(row.newDate);
+    });
+  })(rows)
 }
 
-// const newDate = getDateFromAppleDate(366570014);
-// setDate(newDate);
-
-var i = 1;
-
-// replace with all
-
 db.serialize(() => {
-  db.each(`
+  db.all(`
     SELECT ZADDITIONALASSETATTRIBUTES.Z_PK as pkAdd, 
            ZGENERICASSET.Z_PK as pkAss,
            ZORIGINALFILENAME as filename, 
@@ -57,15 +45,13 @@ db.serialize(() => {
     ON ZGENERICASSET.Z_PK = Z_26ASSETS.Z_34ASSETS
     INNER JOIN ZGENERICALBUM
     ON Z_26ASSETS.Z_26ALBUMS = ZGENERICALBUM.Z_PK
-    -- WHERE ZGENERICALBUM.ZTITLE = "2005 Sölden" AND ZGENERICASSET.ZUNIFORMTYPEIDENTIFIER = "public.mpeg"
-    WHERE ZGENERICALBUM.ZTITLE = "Broken Videos" --AND ZGENERICASSET.ZUNIFORMTYPEIDENTIFIER = "public.mpeg"
-    -- WHERE ZORIGINALFILENAME = "MVI_0711.MOV"
-    `, (err, row) => {
+    WHERE ZGENERICALBUM.ZTITLE = "Broken Videos"
+    `, (err, rows) => {
       if (err) {
         console.error(err.message);
       }
       // console.log(i++ + "\t" + row.pkAss + "\t" + row.pkAdd + "\t" + row.filename + "\t" + row.albumTitle + "\t" + row.fileCreatedDate + "\t" + row.date + "\t" + row.videoType + "\t" + row.id);
-      setDate(row.id, getDateFromAppleDate(row.fileCreatedDate));
+      setDate(rows.map(row => { return { id: row.id, newDate: getDateFromAppleDate(row.fileCreatedDate) }}));
   });
 });
 
@@ -76,3 +62,13 @@ db.close((err) => {
   console.log('Close the database connection.');
 });
 
+// some notes:
+// clear recently deleted items first => those will be recognised as well by the query, but not by jxa
+
+// 366570014 backup date created 13.08.2012 19:00:14
+// 366570014000
+// 0 => 01.01.2001 1:00:00
+// -31622400 => 01.01.2000 1:00:00
+//  31536000 => 01.01.2002 1:00:00
+
+// Finnland Photos haben das Datum im Namen
